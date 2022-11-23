86e6adc13fa7e0566041c28a8b7e39e6cfe0095f
Ishamael <rikonvald@gmail.com>
2022-11-23 15:50:19 +0300

    Добавьте файлы проекта.

---
+++ SpotycachAPI/Controllers/ContactsController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SpotycachAPI.Data;
+using SpotycachAPI.Models;
+
+namespace SpotycachAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : Controller
+    {
+        private readonly ContactsApiDbContext _context;
+
+        public ContactsController(ContactsApiDbContext dbContext)
+        {
+            this._context = dbContext;
+        }
+
+        
+        [HttpGet]
+        public async Task<IActionResult> GetContacts()
+        {
+            return Ok(await _context.Contacts.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddContact(AddContactRequest addContactRequest)
+        {
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                Address = addContactRequest.Address,
+                Email = addContactRequest.Email,
+                FullName = addContactRequest.FullName,
+                Phone = addContactRequest.Phone
+            };
+            await _context.Contacts.AddAsync(contact);
+            await _context.SaveChangesAsync();
+
+            return Ok(contact);
+
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateContact([FromRoute] Guid id, UpdateContactRequest updateContactRequest)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+            
+            if (contact != null)
+            {
+                contact.Address = updateContactRequest.Address;
+                contact.Email = updateContactRequest.Email;
+                contact.FullName = updateContactRequest.FullName;
+                contact.Phone = updateContactRequest.Phone;
+                
+                await _context.SaveChangesAsync();
+
+                return Ok(contact);
+            }
+
+            
+
+            return NotFound();
+
+        }
+    }
+}
---
+++ SpotycachAPI/Data/ContactsApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpotycachAPI.Models;
+
+namespace SpotycachAPI.Data
+{
+    public class ContactsApiDbContext: DbContext
+    {
+        public ContactsApiDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
