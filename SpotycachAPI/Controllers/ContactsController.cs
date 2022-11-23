de66100d790edd66973a3882c7cfb90e2cb8499b
Ishamael <rikonvald@gmail.com>
2022-11-23 16:10:44 +0300

    123

--- SpotycachAPI/Controllers/ContactsController.cs
+++ SpotycachAPI/Controllers/ContactsController.cs
@@ -23,6 +23,19 @@ namespace SpotycachAPI.Controllers
             return Ok(await _context.Contacts.ToListAsync());
         }
 
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetContactById([FromRoute] Guid id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return Ok(contact);
+        }
+
         [HttpPost]
         public async Task<IActionResult> AddContact(AddContactRequest addContactRequest)
         {
@@ -63,6 +76,22 @@ namespace SpotycachAPI.Controllers
 
             return NotFound();
 
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<ActionResult> DeleteContact([FromRoute] Guid id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+
+            if (contact != null)
+            {
+                _context.Remove(contact);
+                await _context.SaveChangesAsync();
+
+                return Ok(contact );
+            }
+            return NotFound(contact);
         }
     }
 }
