d8a55a6e60aacc989248a34954b7b4481fc62898
Rikovald <rikonvald@gmail.com>
2024-10-13 16:14:03 +0300

    reinit from c# to node + ts

--- SpotycachAPI/Models/Users/UserAuthRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SpotycachAPI.Models.Users
-{
-    public class UserAuthRequest
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-
-    }
-}
